c0c440a7b6f86090b9638cc4c65b2189ab677244
manthanhhabwani <manthanhabwani17@gmail.com>
2026-08-06 22:29:07 +0530

    status: started with JavaScript

---
+++ script.js
@@ -0,0 +1,4 @@
+const noteForm = document.querySelector(".note-form");
+const titleInput = document.querySelector("#title");
+const notesTextarea = document.querySelector("#notes");
+const notesContainer = document.querySelector(".notes-container");
