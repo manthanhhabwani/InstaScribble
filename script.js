e87d198bd884bbc8653e64a9dee74745a2f98c87
manthanhhabwani <manthanhabwani17@gmail.com>
2026-08-10 21:32:07 +0530

    function: making the submit/save  button work properly

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const noteForm = document.querySelector(".note-form");
 const titleInput = document.querySelector("#title");
 const notesTextarea = document.querySelector("#notes");
 const notesContainer = document.querySelector(".notes-container");
+noteForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const title = titleInput.value.trim();
+  const notes = notesTextarea.value.trim();
+
+  console.log(title);
+  console.log(notes);
+});
