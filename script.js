dc216085cd22378349922057fd90a8c5acbfb5df
manthanhhabwani <manthanhabwani17@gmail.com>
2026-08-11 19:02:54 +0530

    feature: add time stamp for the notes

--- script.js
+++ script.js
@@ -15,15 +15,21 @@ noteForm.addEventListener("submit", function (event) {
   console.log(title);
   console.log(notes);
 
+  const createdAt = new Date();
+  const dateText = createdAt.toLocaleDateString();
   const note = {
     title: title,
     content: notes,
+    createdAt: new Date()
   };
 
   const article = document.createElement("article");
   article.classList.add("note-card");
   article.innerHTML = `
+  <header class="note-header">
   <h3>${note.title}</h3>
+  <time>${note.createdAt.toLocaleDateString()}</time>
+  </header>
   <p>${note.content}</p>
   <footer class="note-actions">
     <button type="button">Delete</button>
