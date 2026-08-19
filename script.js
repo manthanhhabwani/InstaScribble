c8ccf4f0d3c137d23507c4f6e97b764e12444ac3
manthanhhabwani <manthanhabwani17@gmail.com>
2026-08-19 21:04:13 +0530

    update: notes loads from the local storage

--- script.js
+++ script.js
@@ -2,7 +2,38 @@ const noteForm = document.querySelector(".note-form");
 const titleInput = document.querySelector("#title");
 const notesTextarea = document.querySelector("#notes");
 const notesContainer = document.querySelector(".notes-container");
-const notes = [];
+let notes = [];
+
+const savedNotes = localStorage.getItem("notes");
+
+if (savedNotes !== null) {
+  notes = JSON.parse(savedNotes);
+
+  notes.forEach(function (note) {
+    const article = document.createElement("article");
+    article.classList.add("note-card");
+
+    article.innerHTML = `
+  <header class="note-header">
+    <h3>${note.title}</h3>
+    <time>${note.createdAt}</time>
+  </header>
+
+  <p>${note.content}</p>
+
+  <footer class="note-actions">
+    <button type="button">Delete</button>
+  </footer>
+`;
+
+    notesContainer.append(article);
+
+    const deleteButton = article.querySelector("button");
+    deleteButton.addEventListener("click", function () {
+      article.remove();
+    });
+  });
+}
 noteForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
@@ -13,9 +44,6 @@ noteForm.addEventListener("submit", function (event) {
     return;
   }
 
-  console.log(title);
-  console.log(noteContent);
-
   const createdAt = new Date();
   const dateText = createdAt.toLocaleDateString();
   const note = {
@@ -25,8 +53,6 @@ noteForm.addEventListener("submit", function (event) {
   };
 
   notes.push(note);
-
-  JSON.stringify(notes);
 
   localStorage.setItem("notes", JSON.stringify(notes));
 
